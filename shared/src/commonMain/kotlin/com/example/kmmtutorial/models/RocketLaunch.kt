5dac85849e47ab82b96a047d8e4625ed7e11e07f
Aslan Assylkhanov <AssylkhanovAslan@gmail.com>
2023-03-28 17:53:01 +0600

    Corrected Serialization

--- shared/src/commonMain/kotlin/com/example/kmmtutorial/models/RocketLaunch.kt
+++ shared/src/commonMain/kotlin/com/example/kmmtutorial/models/RocketLaunch.kt
@@ -5,8 +5,12 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class RocketLaunch (
+    @SerialName("flight_number")
     val flightNumber: Int,
+    @SerialName("name")
     val missionName: String,
+    @SerialName("date_utc")
     val launchDateUTC: String,
+    @SerialName("success")
     val launchSuccess: Boolean?,
 )
