17778458ce6d5e1e440178a76af7c995246e837d
Aslan Assylkhanov <AssylkhanovAslan@gmail.com>
2023-03-29 16:05:50 +0600

    Added very basic ios ui

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -5,32 +5,32 @@ struct ContentView: View {
     @ObservedObject private(set) var viewModel: ViewModel
 
         var body: some View {
-            Text(viewModel.text)
+            List(viewModel.posts, id: \.id) {post in
+                Spacer().padding(16)
+                VStack(spacing: 16){
+                    Text(post.title)
+                    
+                    Text(post.body)
+                }
+                .cornerRadius(8)
+            }
+            .padding(16)
         }
 }
 
 extension ContentView {
     class ViewModel: ObservableObject {
         @Published var text = "Loading..."
+        @Published var posts = [Post]()
         init() {
             PostsRepository().getPosts {
-                posts, error in
+                postsList, error in
                 DispatchQueue.main.async {
-                    if let greeting = posts?.first {
-                        self.text = greeting.title
-                    } else {
-                        self.text = error?.localizedDescription ?? "error"
+                    if let postsList {self.posts += postsList} else {
+                        
                     }
                 }
             }
-//            Greeting().greeting { greeting, error in
-//            DispatchQueue.main.async {
-//                if let greeting = greeting {
-//                    self.text = greeting
-//                } else {
-//                    self.text = error?.localizedDescription ?? "error"
-//                }
-//            }
         
         }
     }
